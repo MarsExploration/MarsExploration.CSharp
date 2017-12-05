81b5ed5603766d95361bce323c39cb04fc45c7a3
fgNv <felipe.underlost@gmail.com>
2017-12-05 00:44:20 -0200

    domain classes summaries

--- MarsExploration.Domain/Models/Coordinates.cs
+++ MarsExploration.Domain/Models/Coordinates.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MarsExploration.Domain.Models
 {
+    /// <summary>
+    /// Representação de coordenadas cartesianas
+    /// </summary>
     public struct Coordinates
     {
         [Range(0, Int32.MaxValue)]
